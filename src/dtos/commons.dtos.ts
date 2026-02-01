0b670ff4ff05ad1880941bb94763e1792c2d75fc
mika_sampaio <mikaeli.sampaio@sharkdev.com.br>
2026-02-01 18:12:33 -0300

    feat: Adiciona valores padrão para paginação no esquema de consulta e define tipos de requisição para o Express

--- src/dtos/commons.dtos.ts
+++ src/dtos/commons.dtos.ts
@@ -2,8 +2,8 @@ import z from "zod";
 
 export const querySchema = z.object({
   id: z.string().optional(),
-  page: z.number().or(z.string()).optional(),
-  pageSize: z.number().or(z.string()).optional(),
+  page: z.number().or(z.string()).default(1).optional(),
+  pageSize: z.number().or(z.string()).default(10).optional(),
   search: z.string().optional(),
 });
 
--- src/interfaces/commons.interface.ts
+++ src/interfaces/commons.interface.ts
@@ -1,4 +1,9 @@
+import type { Request } from "express";
+
 export type IGetResponse<T> = {
   results: T[];
   total: number;
 };
+
+export type BodyRequest<T> = Request<unknown, unknown, T>;
+export type QueryRequest<T> = Request<unknown, unknown, unknown, T>;
